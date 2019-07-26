fe89629d803e4bcd9fbc74c962018b0e04267ca5
Tutku Çakır <tutku.cakir123@gmail.com>
2019-07-26 23:59:47 +0300

    Form size has changed - Test Commit

--- MyLittleBookshelf/Form1.Designer.cs
+++ MyLittleBookshelf/Form1.Designer.cs
@@ -266,7 +266,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Lavender;
-            this.ClientSize = new System.Drawing.Size(1179, 690);
+            this.ClientSize = new System.Drawing.Size(1079, 698);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.flowLayoutPanel1);
             this.Controls.Add(this.panel1);
